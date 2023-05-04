16ef5592ba9165eb1464eb1c4bf1b73eb5f40eca
larrysaam <nyaah.saam@ictuniversity.edu.cm>
2023-05-04 14:35:28 +0100

    home page side navigation bar

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import NavigationBar from './navigation';
+import './navigation.css';
+import {useState} from 'react';
+
+function App() {
+
+  return(
+    <div className='homeDiv'>
+        <NavigationBar />
+    </div>
+
+  )
+}
+
+export default App;
